119c8930161046304360944b08ac54d5b71aa40a
ClausHofman <46158@edu.raseko.fi>
2022-10-19 18:53:30 +0300

    3.2

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import React, { useState } from 'react'
 import TodoList from "./TodoList"
 
 function App() {
-  const [todos, setTodos] = useState([]) // object destructuring. useState returns an array with two elements. The first element is the value of the state, the second element is a function that allows us to update the state. We can name these elements whatever we want, but it's common to name them after the state variable and the function that updates it. In this case, the state variable is todos and the function that updates it is setTodos.
+  const [todos, setTodos] = useState(['Todo 1', 'Todo 2']) // object destructuring. useState returns an array with two elements. The first element is the value of the state, the second element is a function that allows us to update the state. We can name these elements whatever we want, but it's common to name them after the state variable and the function that updates it. In this case, the state variable is todos and the function that updates it is setTodos.
   return (
     <>
-    <TodoList />
+    <TodoList todos={todos} />  {/* props are passed in as an object. The key is the name of the prop and the value is the value of the prop. */} 
     <input type="text" />
     <button>Add Todo</button>
     <button>Clear Completed</button>
