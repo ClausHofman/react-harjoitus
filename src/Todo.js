c01bd5bed3d4813f5feaaa4cee31b331c40c0259
ClausHofman <46158@edu.raseko.fi>
2022-10-19 19:06:07 +0300

    create Todo component

---
+++ src/Todo.js
@@ -0,0 +1,9 @@
+import React from 'react'
+// "rfc" shortcut from VS Code extension ES7 React/Redux/GraphQL/React-Native snippets
+export default function Todo({ todo }) {
+  return (
+    <div>
+      {todo}
+    </div>
+  )
+}
