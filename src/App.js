feccb77355edeb361bcbc52365e0d9e0de6e7934
ClausHofman <46158@edu.raseko.fi>
2022-10-21 20:05:13 +0300

    setTodos

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useRef } from 'react' // useRef allows us to access the DOM node that we're referencing. We can use it to get the value of the input field.
 import TodoList from "./TodoList"
+import { v4 as uuidv4 } from 'uuid' // uuid is a library that generates unique ids. We're importing the v4 function from the uuid library and renaming it to uuidv4. We can now use uuidv4 to generate unique ids.
 
 function App() {
   const [todos, setTodos] = useState([]) // object destructuring. useState returns an array with two elements. The first element is the value of the state, the second element is a function that allows us to update the state. We can name these elements whatever we want, but it's common to name them after the state variable and the function that updates it. In this case, the state variable is todos and the function that updates it is setTodos.
@@ -7,7 +8,10 @@ function App() {
   function handleAddTodo(e){      {/* handleAddTodo is a function that will be called when the user clicks the Add Todo button. */}
     const name = todoNameRef.current.value
     if (name === '') return
-    console.log(name) // test to see if the function is working
+    setTodos(prevTodos => {
+      return [...prevTodos, { id: uuidv4(), name: name, complete: false }]
+    })  
+    todoNameRef.current.value = null
   }
   return (
     <>
